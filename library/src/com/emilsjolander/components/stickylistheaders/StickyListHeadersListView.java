b40d58e6d0bc5179c5835fe492103baaeb2d6001
Hensh <ilya.sergeev@me.com>
2013-07-23 23:44:50 +0400

    update android_support_v4

--- library/src/com/emilsjolander/components/stickylistheaders/StickyListHeadersListView.java
+++ library/src/com/emilsjolander/components/stickylistheaders/StickyListHeadersListView.java
@@ -10,7 +10,6 @@ import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
